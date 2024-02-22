17194eb4be92b371525e47f08b2a871ec04770d8
Crowdbotics <team@crowdbotics.com>
2024-02-22 14:05:23 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "falling-cloud-46570",
+  url: "https://falling-cloud-46570.botics.co",
+  api: "https://falling-cloud-46570.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
